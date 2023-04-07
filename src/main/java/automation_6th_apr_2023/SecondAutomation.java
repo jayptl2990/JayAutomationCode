ad79cb0eec252dcd6a13c570fb6802619ff73989
jayptl2990 <jayptl2990@gmail.com>
2023-04-07 03:05:43 -0700

    forth commit

---
+++ src/main/java/automation_6th_apr_2023/SecondAutomation.java
@@ -0,0 +1,13 @@
+package automation_6th_apr_2023;
+
+public class SecondAutomation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		System.out.println("hello foxx");
+	}
+
+}
